cc019ac8a2d45b101f8abb381988d5f0e765877e
Mohammadreza <m.zandieh1999@gmail.com>
2025-05-29 14:07:16 +0330

    set up CoinsListViewModel and its state and UiCoinListItem - part 5,6

---
+++ composeApp/src/commonMain/kotlin/org/example/project/app/coins/presentation/CoinsListViewModel.kt
@@ -0,0 +1,59 @@
+package org.example.project.app.coins.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import org.example.project.app.coins.domain.GetCoinsListUseCase
+import org.example.project.app.core.domain.Result
+
+class CoinsListViewModel(
+    private val getCoinsListUseCase: GetCoinsListUseCase,
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinsState())
+    val state = _state
+        .onStart {
+            getAllCoins()
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = CoinsState()
+        )
+
+    private suspend fun getAllCoins() {
+        when (val coinsResponse = getCoinsListUseCase.execute()) {
+            is Result.Success -> {
+                _state.update {
+                    CoinsState(
+                        coins = coinsResponse.data.map { coinItem ->
+                            UiCoinListItem(
+                                id = coinItem.coin.id,
+                                name = coinItem.coin.name,
+                                iconUrl = coinItem.coin.iconUrl,
+                                symbol = coinItem.coin.symbol,
+                                formattedPrice = coinItem.price.toString(), //TODO change it later
+                                formattedChange = coinItem.price.toString(), // TODO change it later
+                                isPositive = coinItem.change >= 0,
+                            )
+                        }
+                    )
+                }
+            }
+
+            is Result.Error -> {
+                _state.update {
+                    it.copy(
+                        coins = emptyList(),
+                        error = null,
+                    )
+                }
+            }
+        }
+    }
+
+
+}
