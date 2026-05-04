ab7a5023dc16c3a57c3619aefc8d4005b317e1bf
Mohammadreza <m.zandieh1999@gmail.com>
2026-05-04 13:15:57 +0330

    change repositories in settings.gradle.kts to using myket

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,28 +3,38 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+
+        /* MYKET REPOSITORY */
+        maven {url = uri("https://maven.myket.ir")}
+
+
+//        google {
+//            mavenContent {
+//                includeGroupAndSubgroups("androidx")
+//                includeGroupAndSubgroups("com.android")
+//                includeGroupAndSubgroups("com.google")
+//            }
+//        }
+//        mavenCentral()
+//        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
+
+        /* MYKET REPOSITORY */
+        maven {url = uri("https://maven.myket.ir")}
+
+
+//        google {
+//            mavenContent {
+//                includeGroupAndSubgroups("androidx")
+//                includeGroupAndSubgroups("com.android")
+//                includeGroupAndSubgroups("com.google")
+//            }
+//        }
+//        mavenCentral()
     }
 }
 
